9bddc8116252aad3cc09096d7af1503bbb7dd4a6
Dalke <adalke@havigs.com>
2016-03-23 05:31:05 -0500

    Completed first two questions of Chapter 2 exercises

---
+++ Exercises_Chapter2.R
@@ -0,0 +1,16 @@
+### Chapter 2 Exercises ###
+
+#1.a.) Better - With the small p, the risk of overfitting is relatively small,
+        #and the large sample size will allow a flexible model to more accurately
+        #approximate the training data.
+#1.b.) Worse - The small n and large p risk overfitting with a flexible model
+#1.c.) Better - A more flexible model will capture the non-linear nature of this data
+#1.d.) Worse - A flexible model might overfit the high-variance data
+
+#2.a.) n = 500, p = profit, number of employees, industry; regression; inference
+#2.b.) n = 20, p = price, marketing budget, competition price, 10 other variables; 
+      #classification; prediction
+#2.c.) n = 52, p = % US market change, % British market change, 
+      #% German market change; regression; prediction
+
+#3.a.)
