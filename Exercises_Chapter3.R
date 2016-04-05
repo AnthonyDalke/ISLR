7037f927f32d0995e4f8637ca988a3faec9dcb2e
Anthony Dalke <anthony.dalke@gmail.com>
2016-04-05 05:28:31 -0500

    Began chapter 3 exercises on Mac.

---
+++ Exercises_Chapter3.R
@@ -0,0 +1,9 @@
+# 1.) All the predictor variables except NEWSPAPER have statistically significant effects on the model.
+# In other words, holding the other variables constant, each variable helps explain the relationship between
+# the predictor and dependent variables.
+
+# 3.) a.) The interaction variable between GPA and gender makes the difference here: for a high enough female
+# GPA, the negative coefficient pushes the predicted salary below that of a male, so "iii".
+# b.)
+50 + (20 * 4) + (0.07 * 110) + 35 + (0.01 * 110 * 4) - (10 * 4)
+# c.) False - the deciding piece of information concerns the variable's p-value, not simply its coefficient.
