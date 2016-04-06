31ebb469e55c2816b640f35bbbc5ffd8b352e1a7
Anthony Dalke <anthony.dalke@gmail.com>
2016-04-06 05:38:56 -0500

    Finished conceptual questions.

--- Exercises_Chapter3.R
+++ Exercises_Chapter3.R
@@ -7,3 +7,18 @@
 # b.)
 50 + (20 * 4) + (0.07 * 110) + 35 + (0.01 * 110 * 4) - (10 * 4)
 # c.) False - the deciding piece of information concerns the variable's p-value, not simply its coefficient.
+
+# 4.) a.) Because the data set has a linear relationship, the linear model should have a lower RSS
+# b.) Since higher-dimension models have a tendency to overfit on training data, the linear model should
+# have a lower RSS
+# c.) In this case, the cubic model should have a lower RSS, because its higher dimensions should enable
+# it to capture the non-linear relationship in the data
+# d.) Again, since higher-dimension models tend to overfit, the linear model should have a lower RSS
+
+# 5.) a-prime reflects the product of fitted x values and the accompanying point's derivative (slope)
+
+# 6.) Since linear regression aims to minimize the distance between fitted values and x-bar and y-bar,
+# it will strive to come as close as possible to (x-bar, y-bar).
+
+# 7.) Both the linear regression and correlational coefficient formulas depend on the distance between
+# fitted values and the averages of x and y.
